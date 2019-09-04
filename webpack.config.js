e6b2326668e82c30c5ed931c4e72ee769e6746e6
Nahum Obregon <nahumobregon@gmail.com>
2019-09-04 15:33:00 -0500

    Corrigiendo webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 const path = require('path');
 
-module.exports ={
-	devtool: 'source-map',
-	context: path.join(__dirname, 'src'),
-	entry: [
-		'./main.js'
-	],
-	output:{
-		path: path.join(__dirname,'www'),
-		filename: 'bundle.js',
-		publicPath: '/dist'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loaders:[
-					'babel-loader'
-				]
-			}
-		]
-	},
-	resolve: {
-		module: [
-			path.resolve(__dirname,'src'),'node_modules'
-		]
-	}
+module.exports = {
+  devtool: 'source-map',
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './main.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules')
+    ]
+  }
 };
